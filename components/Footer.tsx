fe4526f59abe0d3dd077659c6690bdc126083ffc
Mark <mark.nelubin@gmail.com>
2023-01-12 21:33:41 +0700

    Added Footer to Sidebar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { footerList1, footerList2, footerList3 } from "../utils/constants";
+import type { AppProps } from "next/app";
+
+type ListProps = { items: string[]; mt: boolean };
+
+const List = ({ items, mt }: ListProps) => {
+  return (
+    <div className={`flex flex-wrap gap-2 ${mt && "mt-5"} `}>
+      {items.map((item) => (
+        <p
+          key={item}
+          className="text-gray-400 text-sm hover:underline cursor-pointer"
+        >
+          {item}
+        </p>
+      ))}
+    </div>
+  );
+};
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mt-6 hidden xl:block">
+      <List items={footerList1} mt={false} />
+      <List items={footerList2} mt />
+      <List items={footerList3} mt />
+      <p className="text-gray-400 text-small mt-5">2023 TikTik</p>
+    </div>
+  );
 };
 
 export default Footer;
