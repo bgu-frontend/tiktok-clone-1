191a00485bd6b8dfb03df58ae9c9727eb5684696
Mark <mark.nelubin@gmail.com>
2023-01-12 19:12:01 +0700

    Sidebar with Toggle, Home and Login btn

---
+++ components/index.tsx
@@ -0,0 +1,7 @@
+import SuggestedAccounts from "./SuggestedAccounts";
+import Discover from "./Discover";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+
+export { SuggestedAccounts, Discover, Footer, Navbar, Sidebar };
