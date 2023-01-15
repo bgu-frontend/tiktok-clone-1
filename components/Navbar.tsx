09ccb21133f1db7ee4f6567378c695c454204a46
Mark <mark.nelubin@gmail.com>
2023-01-15 11:43:55 +0700

    Added buttons for Logged user in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import { GoogleLogin, googleLogout } from "@react-oauth/google";
-// import { useRouter } from "next/router";
-// import { AiOutlineLogout } from "react-icons/ai";
-// import { BiSearch } from "react-icons/bi";
-// import { IoMdAdd } from "react-icons/io";
+import { useRouter } from "next/router";
+import { AiOutlineLogout } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+import { IoMdAdd } from "react-icons/io";
 import Logo from "../utils/tiktik-logo.png";
 import { createOrGetUser } from "../utils";
 import useAuthStore from "../store/authStore";
@@ -26,10 +26,36 @@ const Navbar = () => {
         </div>
       </Link>
       <div>SEARCH</div>
-
       <div>
-        {user ? (
-          <div>Logged in</div>
+        {/* Buttons for logged user */}
+        {userProfile ? (
+          <div className="flex gap-5 md:gap-10">
+            {/* Upload button */}
+            <Link href="/upload">
+              <button className="flex items-center gap-2 border-2 px-2 md:px-4 text-md font-semibold ">
+                <IoMdAdd className=" text-xl" />
+                <span className="hidden md:block">Upload</span>
+              </button>
+            </Link>
+            {/* User Profile Image */}
+            {userProfile.image && (
+              <Link href="/">
+                <>
+                  <Image
+                    width={40}
+                    height={40}
+                    className=" cursor-pointer rounded-full"
+                    src={userProfile.image}
+                    alt="profile photo"
+                  />
+                </>
+              </Link>
+            )}
+            {/* Logout button */}
+            <button type="button" className="px-2">
+              <AiOutlineLogout color="red" fontSize={28} />
+            </button>
+          </div>
         ) : (
           <GoogleLogin
             onSuccess={(res) => {
