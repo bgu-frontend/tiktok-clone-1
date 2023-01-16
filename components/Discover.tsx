356168e1c6deda8d01254d9f9103a080c1f18b88
Mark <mark.nelubin@gmail.com>
2023-01-16 19:48:07 +0700

    little refactor on styles

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { MouseEventHandler, useState } from "react";
 import { topics } from "../utils/constants";
 
 const Discover = () => {
@@ -8,10 +10,17 @@ const Discover = () => {
     query: { topic },
   } = router;
 
-  const activeTopicStyle =
-    "xl:border-2 hover:bg-primary xl:border-[var(--primary-color)] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[var(--primary-color)]";
-  const topicStyle =
-    "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black";
+  const [activeCategory, setActiveCategory] = useState("");
+  useEffect(() => {
+    if (!topic) {
+      setActiveCategory("");
+    }
+  }, [topic]);
+
+  const basicStyle =
+    "xl:border-2 hover:bg-primary px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer";
+  const activeTopicStyle = `${basicStyle} xl:border-[var(--primary-color)] text-[var(--primary-color)]`;
+  const topicStyle = `${basicStyle} xl:border-gray-300 text-black`;
 
   return (
     <div className="xl:border-b-2 xl:border-fray-200 pb-6">
@@ -22,7 +31,10 @@ const Discover = () => {
         {topics.map((item) => (
           <Link href={`/?topic=${item.name}`} key={item.name}>
             <div
-              className={topic === item.name ? activeTopicStyle : topicStyle}
+              className={
+                activeCategory === item.name ? activeTopicStyle : topicStyle
+              }
+              onClick={() => setActiveCategory(item.name)}
             >
               <span className="text-2xl">{item.icon}</span>
               <span className="font-medium hidden xl:block capitalize">
